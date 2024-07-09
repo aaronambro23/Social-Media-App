0109a5c6e0e86a559493835a230114b0fbf7def0
Aaron Ambrosi <aaronambrosi@Aarons-MacBook-Pro.local>
2024-07-09 20:33:53 +0200

    Changed imports to make it more readable

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -2,19 +2,11 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/@/components/ui/form"
-
 import { Input } from "@/@/components/ui/input"
 import { Button } from "@/@/components/ui/button"
 import { SignupValidation } from "@/lib/validation"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/@/components/ui/form"
+
 
 const SignupForm = () => {
   const isLoading = true
